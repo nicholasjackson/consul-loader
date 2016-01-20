2e42ac80dda5cef33e59dee32a912f3be13ff8a4
Nic Jackson <jackson.nic@gmail.com>
2016-01-20 23:06:29 +0000

    added code climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ Bundler.setup
 
 require 'webmock/rspec'
 require 'consul_loader' # and any other gems you need
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 
 RSpec.configure do |config|
   # some (optional) config here
